9bbe95e124100a9fa42f007bee5d60ac673e809b
Ribhav <ribhavsharma2003@gmail.com>
2018-11-07 02:41:03 +0530

    add splash screen

---
+++ app/src/main/java/com/community/jboss/leadmanagement/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.community.jboss.leadmanagement;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, BaseActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
+    }
+}
